07d1c25ca5eac13fd624b5655ac48a23670188ab
Artem Manakov <artem.manakov0@gmail.com>
2020-08-26 18:12:33 +0300

    Initial Commit

---
+++ square1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  square1
+//
+//  Created by Artem Manakov on 26.08.2020.
+//  Copyright © 2020 Artem Manakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
