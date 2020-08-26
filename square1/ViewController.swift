2c1c1530553b83f8175af841db5e721b3411b7ba
Artem Manakov <artem.manakov0@gmail.com>
2020-08-26 18:14:55 +0300

    Task1

--- square1/ViewController.swift
+++ square1/ViewController.swift
@@ -13,8 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        makeBoxes(howManySquaresOnTheGroundFloor: 4)
     }
-
+    
+    func makeBoxes(howManySquaresOnTheGroundFloor: Int) {
+        var howManySquaresOnTheFloor = howManySquaresOnTheGroundFloor
+        var x = 0
+        while howManySquaresOnTheFloor != 0 {
+            let box = UIView()
+            box.frame.size.width = 64
+            box.frame.size.height = 64
+            x += 72
+            box.frame.origin.x = CGFloat(x)
+            box.frame.origin.y = 256
+            box.backgroundColor = .brown
+            view.addSubview(box)
+            howManySquaresOnTheFloor -= 1
+        }
+        
+    }
 
 }
 
